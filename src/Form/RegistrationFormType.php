435caa7b378434fd3db1db39d44365233895b7ef
danngalann <47415745+danngalann@users.noreply.github.com>
2020-12-03 15:36:15 +0100

    Some translations

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,25 +18,17 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('username')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Por favor introduce una contraseña',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Tu contraseña debería tener como mínimo {{ limit }} carácteres',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
